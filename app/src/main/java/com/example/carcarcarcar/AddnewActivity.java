130f3576bf84cb8dcbf3643eda577ae7a4dec6b0
Seung Nayeon <sny3238@gmail.com>
2020-05-17 20:03:56 +0900

    history activity 수정

--- app/src/main/java/com/example/carcarcarcar/AddnewActivity.java
+++ app/src/main/java/com/example/carcarcarcar/AddnewActivity.java
@@ -1,13 +1,11 @@
 package com.example.carcarcarcar;
 
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
